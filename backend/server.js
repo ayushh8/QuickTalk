8eca2f2d727c3e88659293cb72e7ff2d41effa6f
ayushh8 <145754355+ayushh8@users.noreply.github.com>
2024-08-28 17:11:37 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config(); 
+
+// app.get("/", (req, res) => {
+//     res.send("Hello server");
+// });
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+
+app.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`)
+});
